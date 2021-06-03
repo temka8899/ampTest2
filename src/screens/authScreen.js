bdecf43bf87949e2546159f9db1e125b57663330
temka8899 <temuleon8899@gmail.com>
2021-06-03 16:09:51 +0800

    temkea

--- src/screens/authScreen.js
+++ src/screens/authScreen.js
@@ -307,7 +307,7 @@ const SignInScreen = ({navigation, onPress}) => {
         'response:>>',
         response.signInUserSession.accessToken.jwtToken,
       );
-      navigation.replace('createGameScreen');
+      navigation.replace('Home');
       console.log('✅ Sign In Success');
       setUsername('');
       setPassword('');
