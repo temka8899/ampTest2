1db2158440e47c3440afebaa2c1481729d2e2c59
Munkh-Erdene <moogii67890@gmail.com>
2021-06-04 15:06:40 +0800

    moogii 6.4 6

--- src/components/GamePicker.js
+++ src/components/GamePicker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   StyleSheet,
   Text,
@@ -8,17 +8,45 @@ import {
 } from 'react-native';
 import {RFPercentage} from 'react-native-responsive-fontsize';
 import {COLORS, FONTS, hp, wp} from '../constants/theme';
+import Amplify, {API, graphqlOperation, Auth, Storage} from 'aws-amplify';
+import {listGames, listLeagues} from '../graphql/queries';
 
 const GamePicker = props => {
-  const OPTIONS = ['Table Soccer', 'NBA 2K21', 'FIFA 2021'];
+  const [GameList, setGameList] = useState([]);
 
-  const option = OPTIONS.map((item, index) => {
+  useEffect(() => {
+    fetchGames();
+  }, []);
+
+  async function fetchGames() {
+    // const user = await Auth.currentUserInfo();
+    // console.log('Attributes =======', user);
+    try {
+      const gameData = await API.graphql(graphqlOperation(listGames));
+      const gameList = gameData.data.listGames.items;
+      setGameList(gameList);
+      console.log('Games>>>>', gameList);
+      //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+      // const user2 = await Auth.currentAuthenticatedUser();
+      // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>', user2);
+      // const result = await Auth.updateUserAttributes(user2, {
+      //   'custom:IntLevel': `5`,
+      //   'custom:Xp': `390`,
+      //   'custom:Name': `Mkoogii`,
+      //   'custom:Admin': `1`,
+      // });
+      //>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    } catch (err) {
+      console.log('error fetching todos', err);
+    }
+  }
+  const option = GameList.map((item, index) => {
     return (
       <TouchableOpacity
         style={styles.option}
         key={index}
-        onPress={() => onPressItem(item)}>
-        <Text style={styles.text}>{item}</Text>
+        onPress={() => onPressItem(item.name)}>
+        <Text style={styles.text}>{item.name}</Text>
       </TouchableOpacity>
     );
   });
