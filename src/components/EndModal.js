08ff69f5e668a4d7ba889cd5eda419925187595d
Munkh-Erdene <moogii67890@gmail.com>
2021-06-10 15:27:54 +0800

    moogii 6.10 last

--- src/components/EndModal.js
+++ src/components/EndModal.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import {View, Text, StyleSheet, TouchableOpacity, Image} from 'react-native';
 import Modal from 'react-native-modal';
+import {RFPercentage} from 'react-native-responsive-fontsize';
 import {images} from '../constants';
 import {COLORS, FONTS, hp, wp} from '../constants/theme';
 
@@ -70,27 +71,88 @@ export const EndModal = props => {
           borderWidth: 2,
           justifyContent: 'center',
           alignItems: 'center',
+          // paddingVertical: hp(1),
         }}>
         <View
-          style={{
-            height: hp(11),
-            flexDirection: 'column',
-            justifyContent: 'space-between',
-          }}>
-          <View>
-            <Text style={{}}>WINNER</Text>
-            <Image source={images.men} style={[styles.image]} />
+          style={
+            {
+              // borderWidth: 1,
+              // borderColor: 'red',
+            }
+          }>
+          <View
+            style={{
+              // borderColor: 'red',
+              // borderWidth: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}>
+            <Text
+              style={{
+                color: COLORS.white,
+                fontFamily: FONTS.brandFont,
+                fontSize: RFPercentage(2.5),
+              }}>
+              WINNER
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                // borderColor: 'red',
+                // borderWidth: 1,
+                alignItems: 'center',
+                width: wp(40),
+                justifyContent: 'space-between',
+                marginTop: hp(2),
+                marginBottom: hp(1),
+              }}>
+              <Image source={images.men} style={[styles.winnerImage]} />
+              <Text style={[{color: COLORS.green}, styles.score]}>
+                {winner.one.point + winner.two.point}
+              </Text>
+              <Image source={images.men} style={[styles.winnerImage]} />
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                // borderColor: 'red',
+                // borderWidth: 1,
+                alignItems: 'center',
+                width: wp(30),
+                justifyContent: 'space-between',
+                marginBottom: hp(2),
+              }}>
+              <Image source={images.men} style={[styles.image]} />
+              <Text style={[{color: COLORS.red}, styles.score]}>
+                {loser.one.point + loser.two.point}
+              </Text>
+              <Image source={images.men} style={[styles.image]} />
+            </View>
           </View>
-          <TouchableOpacity
-            style={styles.modalBtnContainer}
-            onPress={props.EndBtn}>
-            <Text style={styles.modalBtnText}>End Match</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.modalBtnContainer}
-            onPress={props.cancelbtn}>
-            <Text style={styles.modalBtnText}>Cancel</Text>
-          </TouchableOpacity>
+          <View
+            style={{
+              width: wp(80),
+              // height: hp(11),
+              justifyContent: 'space-evenly',
+              flexDirection: 'row',
+            }}>
+            <TouchableOpacity
+              style={styles.modalCancelBtn}
+              onPress={props.cancelbtn}>
+              <Text
+                style={[styles.modalBtnText, {fontSize: RFPercentage(1.7)}]}>
+                Cancel
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.modalBtnContainer}
+              onPress={props.EndBtn}>
+              <Text
+                style={[styles.modalBtnText, {fontSize: RFPercentage(1.6)}]}>
+                End Match
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </View>
     </Modal>
@@ -98,20 +160,37 @@ export const EndModal = props => {
 };
 
 const styles = StyleSheet.create({
+  score: {
+    fontSize: RFPercentage(2, 5),
+    fontFamily: FONTS.brandFont,
+  },
   modalBtnText: {
     color: COLORS.white,
     fontFamily: FONTS.brandFont,
   },
+  modalCancelBtn: {
+    width: wp(32),
+    height: hp(5),
+    borderColor: COLORS.brand,
+    borderWidth: 2,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   modalBtnContainer: {
     backgroundColor: COLORS.brand,
-    width: wp(50),
-    height: hp(4),
+    width: wp(32),
+    height: hp(5),
     justifyContent: 'center',
     alignItems: 'center',
   },
   image: {
     width: wp(9.6),
     height: hp(4.43),
-    marginTop: wp(2),
+    resizeMode: 'contain',
+  },
+  winnerImage: {
+    resizeMode: 'contain',
+    width: wp(12),
+    height: hp(5.43),
   },
 });
--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -116,8 +116,11 @@ const GameScreen = ({navigation}) => {
 
   const findGreet = () => {
     const hrs = new Date().getHours();
+    const day = new Date().getDay();
+    console.log(day);
     if (hrs === 0 || hrs < 12) return setGreet('Morning');
     if (hrs === 1 || hrs < 17) return setGreet('Afternoon');
+
     setGreet('Evening');
   };
 
