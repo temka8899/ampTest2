cb3b2303e22f6f66bf7858d465a89092ed99e61b
Munkh-Erdene <moogii67890@gmail.com>
2021-06-15 12:20:48 +0800

    Moogii components CLEAN

--- src/components/CircleXp.js
+++ src/components/CircleXp.js
@@ -1,19 +1,11 @@
 import * as React from 'react';
-import {
-  Easing,
-  TextInput,
-  Animated,
-  Text,
-  View,
-  StyleSheet,
-  Image,
-} from 'react-native';
-// import Svg, {Circle} from 'react-native-svg';
-import {COLORS, images} from '../constants';
-import {hp, wp} from '../constants/theme';
-import {userData} from '../data/Players';
-import {AnimatedCircularProgress} from 'react-native-circular-progress';
+import {View, Image, StyleSheet} from 'react-native';
+
+import {COLORS} from '../constants';
 import {AuthContext} from '../../App';
+import {hp, wp} from '../constants/theme';
+
+import {AnimatedCircularProgress} from 'react-native-circular-progress';
 
 const CircleXp = props => {
   const {userInfo, setUserInfo} = React.useContext(AuthContext);
@@ -29,30 +21,24 @@ const CircleXp = props => {
         rotation={0}
         duration={750}
         lineCap="round"
-        style={{
-          zIndex: -2,
-          // position: 'absolute',
-        }}
         tintColor={COLORS.brand}
         onAnimationComplete={() => console.log('onAnimationComplete')}
         backgroundColor="#F74C1130">
         {fill => (
           <View>
-            <Image
-              source={userInfo.avatar}
-              style={{
-                width: wp(26.6),
-                height: hp(12.3),
-                resizeMode: 'contain',
-                borderRadius: wp(26.6),
-                // position: 'absolute',
-                // zIndex: 2,
-              }}
-            />
+            <Image source={userInfo.avatar} style={styles.image} />
           </View>
         )}
       </AnimatedCircularProgress>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  image: {
+    width: wp(26.6),
+    height: hp(12.3),
+    resizeMode: 'contain',
+    borderRadius: wp(26.6),
+  },
+});
 export default CircleXp;
