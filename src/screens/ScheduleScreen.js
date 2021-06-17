cfc0321197fe34adfa5e0831c6106df9b808d97c
Munkh-Erdene <moogii67890@gmail.com>
2021-06-17 09:24:58 +0800

    6.17

--- src/screens/ScheduleScreen.js
+++ src/screens/ScheduleScreen.js
@@ -14,11 +14,6 @@ import AppBar from '../components/AppBar';
 import {hp, wp} from '../constants/theme';
 import {COLORS, FONTS, icons} from '../constants';
 import LeaguePicker from '../components/LeaguePicker';
-<<<<<<< HEAD
-// import DatePicker from 'react-horizontal-datepicker';
-=======
-
->>>>>>> 2b3d5cf9e31eb172befd73415de291d193a267c6
 import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 
 const ScheduleScreen = ({navigation, route}) => {
@@ -39,6 +34,7 @@ const ScheduleScreen = ({navigation, route}) => {
     setChooseData(option);
     setLoading(false);
     console.log('League bainuu', option);
+    console.log(`hezee ehlesen`, option.startedDate);
   };
 
   let itemID = 0;
@@ -127,15 +123,6 @@ const ScheduleScreen = ({navigation, route}) => {
         </SkeletonPlaceholder>
       ) : (
         <View>
-<<<<<<< HEAD
-          {/* <DatePicker
-            //a>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-            getSelectedDay={selectedDay}
-            endDate={100}
-            color={'#374e8c'}
-          /> */}
-=======
->>>>>>> 2b3d5cf9e31eb172befd73415de291d193a267c6
           <AppBar />
           <TouchableOpacity
             onPress={() => changeModalVisible(true)}
