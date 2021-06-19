7e138b3cee24f3d0287d357c7f1fb537542f8ded
Buynaa <6uynaa@gmail.com>
2021-06-19 15:22:26 +0800

    Check

--- src/screens/GameScreen.js
+++ src/screens/GameScreen.js
@@ -436,7 +436,7 @@ const GameScreen = ({navigation}) => {
                   styles.greeting,
                   {marginTop: hp(1), fontSize: RFPercentage(2.5)},
                 ]}>
-                {userInfo === null ? 'Hello' : `${userInfo.name}`}
+                {userInfo === undefined ? 'Hello' : `${userInfo.name}`}
               </Text>
             </View>
             <View>
@@ -444,7 +444,7 @@ const GameScreen = ({navigation}) => {
                 onPress={() =>
                   navigation.navigate('Tabs', {screen: 'Profile'})
                 }>
-                {userInfo === null ? null : (
+                {userInfo === undefined ? undefined : (
                   <Image source={userInfo.avatar} style={styles.profileImage} />
                 )}
               </TouchableOpacity>
