ef3c8101718cdb2951bf82dc0aa86987331920c2
temka8899 <temuleon8899@gmail.com>
2021-06-23 14:10:34 +0800

    bat last last

--- src/screens/AdminScreen.js
+++ src/screens/AdminScreen.js
@@ -14,14 +14,11 @@ import {
 } from 'react-native';
 
 import {COLORS, FONTS, icons} from '../constants';
-
 import {hp, wp} from '../constants/theme';
-
 import LoadBtn from '../components/Loading';
 import LottieView from 'lottie-react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {RFPercentage} from 'react-native-responsive-fontsize';
-
 import Amplify, {API, graphqlOperation, Auth} from 'aws-amplify';
 import {
   listGames,
@@ -29,7 +26,6 @@ import {
   listLeagues,
   listTeamPlayers,
   listTeams,
-  listTeamPlayers,
 } from '../graphql/queries';
 import {
   createSchedule,
@@ -192,82 +188,6 @@ const AdminScreen = ({navigation}) => {
     }
     addScheduleLoop(startLeagueId);
   };
-<<<<<<< HEAD
-
-  async function addScheduleLoop(startLeagueId) {
-    try {
-      const teamData = await API.graphql(
-        graphqlOperation(listTeams, {
-          filter: {leagueID: {eq: startLeagueId}},
-        }),
-      );
-      const teams = teamData.data.listTeams.items;
-      const k = teams.length;
-      var nemeh = 1;
-      var hasah = teams.length;
-      var date = new Date();
-      var date2 = date.getDay();
-      let dateNemeh = 0;
-      let tooluur = 1;
-      date.setDate(date.getDate() - 1);
-      console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>', teams);
-      for (var i = 1; i < teams.length; i++) {
-        for (var j = 0; j < hasah - 1; j++) {
-          if (dateNemeh % 4 == 0) {
-            date.setDate(date.getDate() + 1);
-            var date2 = date.getDay();
-            if (date2 == 6) {
-              date.setDate(date.getDate() + 2);
-            }
-
-            const leagueData1 = await API.graphql(
-              graphqlOperation(await listTeamPlayers, {
-                filter: {
-                  teamID: {eq: `${teams[j].id}`},
-                },
-              }),
-            );
-            const _teamPlayers1 = leagueData1.data.listTeamPlayers.items;
-            const playerAvatar1 = [];
-            for (var z = 0; z < _teamPlayers1.length; z++) {
-              playerAvatar1.push(_teamPlayers1[z].player.avatar);
-            }
-
-            const leagueData2 = await API.graphql(
-              graphqlOperation(await listTeamPlayers, {
-                filter: {
-                  teamID: {eq: `${teams[j + nemeh].id}`},
-                },
-              }),
-            );
-            const _teamPlayers2 = leagueData2.data.listTeamPlayers.items;
-            const playerAvatar2 = [];
-            for (var z2 = 0; z2 < _teamPlayers2.length; z2++) {
-              playerAvatar2.push(_teamPlayers2[z2].player.avatar);
-            }
-
-            console.log(
-              `${teams[j].name} vs ${
-                teams[j + nemeh].name
-              }___at ${date.toLocaleDateString()}____`,
-            );
-            addSchedule(
-              teams[j].id,
-              teams[j + nemeh].id,
-              date.toLocaleDateString(),
-              startLeagueId,
-              tooluur,
-              playerAvatar1,
-              playerAvatar2,
-            );
-            dateNemeh++;
-            tooluur++;
-          }
-        }
-      }
-    } catch (err) {
-      console.log(err);
-=======
 
   async function addScheduleLoop(startLeagueId) {
     try {
@@ -342,7 +262,6 @@ const AdminScreen = ({navigation}) => {
       }
     } catch (error) {
       console.log('error :>> ', error);
->>>>>>> 27c57f31ae676407bce8ddc4f1e710adab991b78
     }
   }
 
@@ -352,13 +271,8 @@ const AdminScreen = ({navigation}) => {
     date,
     startLeagueId,
     tooluur,
-<<<<<<< HEAD
-    homeAvatar,
-    awayAvatar,
-=======
     playerAvatar1,
     playerAvatar2,
->>>>>>> 27c57f31ae676407bce8ddc4f1e710adab991b78
   ) {
     try {
       await API.graphql(
@@ -371,13 +285,8 @@ const AdminScreen = ({navigation}) => {
             date: `${date}`,
             leagueID: startLeagueId,
             index: tooluur,
-<<<<<<< HEAD
-            homeImage: homeAvatar,
-            awayImage: awayAvatar,
-=======
             homeImage: playerAvatar1,
             awayImage: playerAvatar2,
->>>>>>> 27c57f31ae676407bce8ddc4f1e710adab991b78
           },
         }),
       );
