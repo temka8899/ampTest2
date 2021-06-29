ee3cfac149cd02942b4b7016819b1cde0470a4e2
Buynaa <6uynaa@gmail.com>
2021-06-29 19:42:14 +0800

    check

--- android/app/src/main/java/com/amptest2/MainApplication.java
+++ android/app/src/main/java/com/amptest2/MainApplication.java
@@ -5,6 +5,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.microsoft.codepush.react.CodePush;
 import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
@@ -18,7 +19,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -33,6 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
         protected String getJSBundleFile() {
             return CodePush.getJSBundleFile();
         }
+
+        
         
         @Override
         protected List<ReactPackage> getPackages() {
@@ -40,6 +42,9 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
+          packages.add(new AppCenterReactNativeCrashesPackage(this.application, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)));
+      packages.add(new AppCenterReactNativeAnalyticsPackage(this.application, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)));
+      packages.add(new AppCenterReactNativePackage(this.application));
           packages.add(new LottiePackage());
           return packages;
         }
